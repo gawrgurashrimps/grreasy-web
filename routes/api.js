a3fbc3be1302a67b078d18d01d28197334b924cc
Zohaib Anwar <zanw0001@student.monash.edu>
2021-07-13 13:11:15 +1000

    Adding Schema file+API file; Stabe Release 0.02

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,43 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
+const mysql = require('mysql');
 
-var conn = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    port: 3306,
-    password : 'root',
-    database : 'new_schema'
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'products',
+  connectionLimit: 10,
+  supportBigNumbers: true
 });
+
+function query(sql, args) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function(err, connection) {
+      if (err) {
+        return reject(err);
+      }
+      connection.query(sql, args, function(err, result) {
+        connection.release();
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result);
+      });
+    });
+  });
+}
+
 router.get('/', function(req, res, next) {
-    conn.connect(function(err){
-        if (err) throw err;
-        conn.query("SELECT * FROM items", function (err, result, fields) {
-          if (err)throw err;
-        res.json(result);
-        });
-      });  
+  const sql = 'SELECT * FROM items';
+  query(sql).then(result => {
+    res.json(result);
+  });
 });
+
+
+
+
 
 module.exports = router;
