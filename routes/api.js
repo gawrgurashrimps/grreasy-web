d90aa9203ad3702a474457dd2f77a7398bee273b
Mariusz Skoneczko <mariusz@skoneczko.com>
2021-07-14 18:33:00 +1000

    Allow API to filter by product name

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,32 @@ const { query } = require('../utils/pool.js');
 
 
 router.get('/', function(req, res, next) {
-  const sql = 'SELECT * FROM items';
-  query(sql).then(result => {
+  const searchFor = req.query.name || "";
+  const sql =
+    `
+    SELECT
+        i.id AS product_id,
+        i.name AS product_name,
+        o.name AS origin_name,
+        i.price AS product_price,
+        i.quantity AS product_quantity,
+        u.name AS unit_name,
+        c.name AS category_name
+    FROM
+        items i JOIN origin o ON (i.origin_id = o.id)
+        JOIN unit u ON (i.fk_unit = u.id)
+        JOIN category c ON (i.fk_category = c.id)
+    WHERE
+        LOWER(i.name) LIKE LOWER(CONCAT('%', ?, '%'))
+    ORDER BY i.id
+    LIMIT 100;
+    `;
+  query(
+    sql,
+    [
+        searchFor
+    ]
+  ).then(result => {
     res.json(result);
   });
 });
